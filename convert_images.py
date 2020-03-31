05ddb156787d2b911358de7ea6a161e7e53f92a0
Andy Harris <ajrharris@gmail.com>
2020-03-31 07:41:14 -0400

    Create PNG versions of SegmentationClassSubset npy files

---
+++ convert_images.py
@@ -0,0 +1,26 @@
+import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+with open('ImageSets/Segmentation/train.txt', 'r') as fp:
+    files_train = [line.rstrip() for line in fp.readlines()]
+
+with open('ImageSets/Segmentation/val.txt', 'r') as fp:
+    files_val = [line.rstrip() for line in fp.readlines()]
+
+files_train = [f for f in files_train if os.path.isfile(os.path.join('SegmentationClassSubset/' + f + '.npy'))]
+files_val = [f for f in files_val if os.path.isfile(os.path.join('SegmentationClassSubset/' + f + '.npy'))]
+files_all = np.array(sorted(list(set(files_train).union(set(files_val)))))
+
+for i, fname in enumerate(files_all):
+    if i % 100 == 0:
+        print('File', i, fname)
+
+    infname = os.path.join('SegmentationClassSubset/' + fname + '.npy')
+    outfname = os.path.join('JPEGSegmentation/' + fname + '.png')
+
+    with open(infname, 'rb') as f:
+        image_array = np.load(f)
+        plt.imsave(outfname, image_array[:,:,0])
